ee067fec33d6f0e0ff12eb39b074dc1886cea5bc
salaverd <sim.alaverdyan.02@gmail.com>
2026-04-06 12:29:02 +0400

    workspace setup

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -15,12 +15,15 @@ int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&led))
+        return 0;
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0)
+        return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (gpio_pin_toggle_dt(&led) < 0)
+            return 0;
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
