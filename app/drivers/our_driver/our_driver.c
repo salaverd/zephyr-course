bfbdd0c9a26822bba2538d6df3618efc409166cd
salaverd <sim.alaverdyan.02@gmail.com>
2026-05-30 21:39:50 +0400

    Lesson6 task1, custom driver

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,98 @@
+#include "zephyr/device.h"
+#include "zephyr/devicetree.h"
+#include "zephyr/logging/log.h"
+#include "zephyr/logging/log_core.h"
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/gpio.h>
+
+#define DT_DRV_COMPAT our_driver
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+struct our_driver_config
+{
+    struct gpio_dt_spec led;
+};
+
+struct our_driver_data
+{
+    bool led_state;
+};
+
+static int sample_fetch_my_impl(const struct device *dev,
+                                enum sensor_channel chan)
+{
+
+    const struct our_driver_config *cfg = dev->config;
+    struct our_driver_data *data = dev->data;
+    /* Encender LED */
+    gpio_pin_set_dt(&cfg->led, 1);
+    data->led_state = true;
+
+    LOG_INF("Led ON  , channel %d", chan);
+    return 0;
+}
+
+static int channel_get_my_impl(const struct device *dev,
+                               enum sensor_channel chan,
+                               struct sensor_value *val)
+{
+
+    const struct our_driver_config *cfg = dev->config;
+    struct our_driver_data *data = dev->data;
+    /* Apagar LED */
+    gpio_pin_set_dt(&cfg->led, 0);
+
+    data->led_state = false;
+
+    LOG_INF("Led OFF , channel %d", chan);
+    return 0;
+}
+
+static DEVICE_API(sensor, api_iomico_lecture) = {
+    .sample_fetch = sample_fetch_my_impl,
+    .channel_get = channel_get_my_impl,
+};
+
+//// Init function
+
+static int init(const struct device *dev)
+{
+    const struct our_driver_config *cfg = dev->config;
+
+    if (!gpio_is_ready_dt(&cfg->led))
+    {
+        LOG_ERR("GPIO not ready");
+        return -ENODEV;
+    }
+
+    gpio_pin_configure_dt(
+        &cfg->led,
+        GPIO_OUTPUT_INACTIVE);
+
+    LOG_INF("Device Initializated ");
+    return 0;
+}
+
+#define CREATE_CONFIG(inst)                                 \
+    static const struct our_driver_config config_##inst = { \
+        .led = GPIO_DT_SPEC_INST_GET(inst, led_gpios),      \
+    };
+
+#define CREATE_DATA(inst) \
+    static struct our_driver_data data_##inst;
+
+#define DEV_INST(inst)     \
+    CREATE_CONFIG(inst)    \
+    CREATE_DATA(inst)      \
+    DEVICE_DT_INST_DEFINE( \
+        inst,              \
+        init,              \
+        NULL,              \
+        &data_##inst,      \
+        &config_##inst,    \
+        POST_KERNEL,       \
+        80,                \
+        &api_iomico_lecture);
+
+DT_INST_FOREACH_STATUS_OKAY(DEV_INST);
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,9 @@
+#include "zephyr/device.h"
+#include "zephyr/devicetree.h"
+#include "zephyr/drivers/sensor.h"
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
 // #define SLEEP_TIME_MS 1000
 
 /* The devicetree node identifier for the "led0" alias. */
@@ -10,6 +12,17 @@
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+namespace {
+    void test(){
+        const struct device* driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+        struct sensor_value val ;
+        auto ret = sensor_sample_fetch(driver);
+        k_sleep(K_MSEC(1000));
+        ret = sensor_channel_get(driver , SENSOR_CHAN_AMBIENT_TEMP, &val) ;
+        LOG_INF("Channel ret %d" , ret) ;
+    }
+}
 
 int main(void)
 {
@@ -22,12 +35,14 @@ int main(void)
         return 0;
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0)
-            return 0;
+        // if (gpio_pin_toggle_dt(&led) < 0)
+        //     return 0;
 
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+        // led_state = !led_state;
+        // LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+        // k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
+        test();
+        k_msleep(1000);
     }
     return 0;
 }
